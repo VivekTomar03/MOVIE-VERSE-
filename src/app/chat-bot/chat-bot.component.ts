04aae63a15d5e356987ed4b0d16596075af6e335
VivekTomar03 <tomarvivek90viv@gmail.com>
2023-07-23 17:26:23 +0530

    dd

--- src/app/chat-bot/chat-bot.component.ts
+++ src/app/chat-bot/chat-bot.component.ts
@@ -1,6 +1,12 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
+interface Movie {
+  description: string;
+  id: string;
+  language: string;
+  poster: string;
+  title: string;
+}
 interface ChatMessage {
   text: string;
   isBotResponse: boolean;
@@ -14,7 +20,8 @@ interface ChatMessage {
 export class ChatBotComponent {
   chatMessages: ChatMessage[] = [];
   userInput: string = '';
-
+  showModal: boolean = false;
+  topRecommendedMovies: Movie[] = [];
   constructor(private http: HttpClient) {}
 
   sendMessage() {
@@ -38,4 +45,39 @@ export class ChatBotComponent {
       }
     );
   }
+
+  openModal() {
+    const data = [
+      {
+        "description": "A thief who enters the dreams of others to steal their secrets !!!!",
+        "id": "64b8f0a1f2375d61072fe65c",
+        "language": "English",
+        "poster": "https://i.etsystatic.com/25433423/r/il/dfc97f/2692744791/il_570xN.2692744791_o45d.jpg",
+        "title": "Inception  2"
+      },
+      {
+        "description": "An alien on Earth loses the only device he can use to communicate with his spaceship. His innocent nature and child-like questions force the country to evaluate the impact of there religious views on there people.",
+        "id": "64b93f9dcfd3a807165053b7",
+        "language": "Hindi",
+        "poster": "https://im.rediff.com/movies/2014/sep/17pk-poll1.jpg",
+        "title": "PK"
+      },
+      {
+        "description": "A young man and woman fall in love during a trip to Europe, but face challenges from their traditional families.",
+        "id": "64bbddc6944f37376e207ecb",
+        "language": "Hindi",
+        "poster": "https://i.pinimg.com/474x/c6/de/94/c6de9410f1c5c689555e840e13b88041.jpg",
+        "title": "Dilwale Dulhania Le Jayenge"
+      }
+    ]
+          this.topRecommendedMovies = data;
+          this.showModal = true;
+       
+  }
+
+  // Function to close the modal
+  closeModal() {
+    this.showModal = false;
+  }
 }
+
